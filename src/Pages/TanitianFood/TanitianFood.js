8a8988bf02e988a8e053b486ab4b48ea8d9c4fcd
Brian Quinney <quinneybrianm@gmail.com>
2024-02-01 10:07:08 -0500

    added national dish, set min-height for main section

--- src/Pages/TanitianFood/TanitianFood.js
+++ src/Pages/TanitianFood/TanitianFood.js
@@ -1,11 +1,19 @@
 import React from "react";
 import './TanitianFood.scss';
+import Fish from '../../Assets/Images/fish.jpg';
 
 const TanitianFood = () => {
     return (
         <>
             <h1>Tanitian Food</h1>
-            <p>Tanitian Food text goes here</p>
+            <h2>Tanitian National Dish - I'a Kulimi Tipolo</h2>
+            <img src={Fish} alt="Lemon Cream Fish" />
+            <p>
+                I'a Kulimi Tipolo - or Lemon Cream Fish is the national dish of Taniti. 
+                This succulent white fish is topped and filled with lemon slices, drenched
+                in a creamy, seasoned cocunut cream sauce, and wrapped in banana leaves,
+                before being roasted to perfection.
+            </p>
         </>
     );
 }
