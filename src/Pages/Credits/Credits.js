d341d4972afef9ee6797088b0f10e3231b987b01
Brian Quinney <quinneybrianm@gmail.com>
2024-02-01 10:31:56 -0500

    fixed credit page to show links to original owners

--- src/Pages/Credits/Credits.js
+++ src/Pages/Credits/Credits.js
@@ -11,28 +11,28 @@ const Credits = () => {
                 <br />Artwork credits can be found by cross-referencing the name given to the artwork on this site with the url listed.
                 <h4>Video sources:</h4>
                 <ul>
-                    <li>Promo video from Hero section - samoa_0: https://www.samoa.travel/Content/Images/home-vids/1950x832%20Website%20Desktop%20Homepage%20video.mp4</li>
+                    <li>Promo video from Hero section - samoa_0: <a href="https://www.samoa.travel/Content/Images/home-vids/1950x832%20Website%20Desktop%20Homepage%20video.mp4">Samoa.travel</a></li>
                 </ul>
                 <h4>Image sources:</h4>
                 <ul>
-                    <li>logo: created by Brian Quinney of Functional Codeaholic</li>
-                    <li>hamburger: created by Brian Quinney of Functional Codeaholic</li>
-                    <li>Social Icons: created by Brian Quinney of Functional Codeaholic</li>
-                    <li>RSS Icon: created by Brian Quinney of Functional Codeaholic</li>
-                    <li>Myguide_0: https://images.myguide-cdn.com/costarica/companies/from-el-castillo-lake-arenal-kayak-sup-tour-with-snacks/large/from-el-castillo-lake-arenal-kayak-sup-tour-with-snacks-1675963.jpg</li>
-                    <li>tonga_0: https://tongatourism.travel/wp-content/uploads/2022/08/%F0%9F%92%80-43.jpg</li>
-                    <li>Tahiti_2: https://www.tahiti.com/images1/banners/home_carousel_04.jpg</li>
-                    <li>dreamstime_0: https://thumbs.dreamstime.com/z/cook-islanders-people-eat-traditional-food-group-south-pacific-islands-banning-foreign-junk-food-imports-favour-111889549.jpg</li>
-                    <li>Tahiti_4: https://www.tahiti.com/images1/banners/home_carousel_02.jpg</li>
-                    <li>Hawaii_1: https://www.gohawaii.com/sites/default/files/background-images/iStock-490484446.jpeg</li>
-                    <li>Weather Icons: https://www.deviantart.com/vclouds/art/VClouds-Weather-Icons-179152045</li>
+                    <li>logo: created by Brian Quinney of <a href="https://functionalcodeaholic.com">Functional Codeaholic</a></li>
+                    <li>hamburger: created by Brian Quinney of <a href="https://functionalcodeaholic.com">Functional Codeaholic</a></li>
+                    <li>Social Icons: created by Brian Quinney of <a href="https://functionalcodeaholic.com">Functional Codeaholic</a>, using logos of respective companies</li>
+                    <li>RSS Icon: created by Brian Quinney of <a href="https://functionalcodeaholic.com">Functional Codeaholic</a> based on standard rss icon</li>
+                    <li>Myguide_0: <a href="https://images.myguide-cdn.com/costarica/companies/from-el-castillo-lake-arenal-kayak-sup-tour-with-snacks/large/from-el-castillo-lake-arenal-kayak-sup-tour-with-snacks-1675963.jpg">MyGuide-cdn.com</a></li>
+                    <li>tonga_0: <a href="https://tongatourism.travel/wp-content/uploads/2022/08/%F0%9F%92%80-43.jpg">TongaTourism.travel</a></li>
+                    <li>Tahiti_2: <a href="https://www.tahiti.com/images1/banners/home_carousel_04.jpg">Tahiti.com</a></li>
+                    <li>dreamstime_0: <a href="https://thumbs.dreamstime.com/z/cook-islanders-people-eat-traditional-food-group-south-pacific-islands-banning-foreign-junk-food-imports-favour-111889549.jpg">Thumbs.DreamsTime.com</a></li>
+                    <li>Tahiti_4: <a href="https://www.tahiti.com/images1/banners/home_carousel_02.jpg">Tahiti.com</a></li>
+                    <li>Hawaii_1: <a href="https://www.gohawaii.com/sites/default/files/background-images/iStock-490484446.jpeg">GoHawaii.com</a></li>
+                    <li>Weather Icons: <a href="https://www.deviantart.com/vclouds/art/VClouds-Weather-Icons-179152045">DeviantArt</a></li>
                 </ul>
             </div>
             <div className="copyright">
-                &copy; 2023 Functional Codeaholic. All Rights reserved.
+                &copy; 2023 <a href="https://functionalcodeaholic.com">Functional Codeaholic</a>. All Rights reserved.
             </div>
             <div className="license">
-                All content not credited to other sources is the exclusive property of Brian Quinney and Functional Codeaholic.
+                All content not credited to other sources is the exclusive property of Brian Quinney and <a href="https://functionalcodeaholic.com">Functional Codeaholic</a>.
                 No license is given for any duplication without express, written consent by Brian Quinney.
             </div>
         </>
